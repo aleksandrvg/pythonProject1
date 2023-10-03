ad0ff398a5f2b7d4061ca0ed5362adb0137c7117
student <aleksandrvg79@gmail.com>
2023-10-03 21:15:17 +0300

    recusia

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 def delSecondElement(array:list):
     return array[::2]
-print(delSecondElement([1,2,3,4,5,6,7,8,9]))
+print(delSecondElement([1, 2, 3, 4, 5, 6, 7, 8, 9]))
 
 print('hi')
+
+
+def fibonachi(n):
+    if n == 0 or n == 1:
+        return n
+    return fibonachi(n - 1) + fibonachi(n - 2)
+print(fibonachi(7))
+
+
+def primer(m):
+    if m == -2 or m == -3:
+        return m
+    return primer(m - 2) + primer(m - 1)
+print(primer(2))
